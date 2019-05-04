aeac7c83674e6acb32ddcc74798c8b5c14ae7332
Rob Hagemans <robhagemans@yahoo.co.uk>
2019-05-04 23:10:32 +0100

    monobit.windows: standardise code

--- monobit/windows.py
+++ monobit/windows.py
@@ -117,58 +117,54 @@ _WEIGHT_MAP = {
     900: 'heavy', # bdf 'ultra-bold' is 'heavy'
 }
 
+_BYTE = 'B'
+_WORD = '<H'
+_LONG = '<L'
 
 
-bytestruct = struct.Struct("B")
-wordstruct = struct.Struct("<H")
-dwordstruct = struct.Struct("<L")
-
-def frombyte(s):
-    return bytestruct.unpack_from(s, 0)[0]
-def fromword(s):
-    return wordstruct.unpack_from(s, 0)[0]
-def fromdword(s):
-    return dwordstruct.unpack_from(s, 0)[0]
+def unpack(format, buffer, offset):
+    """Unpack a single value from bytes."""
+    return struct.unpack_from(format, buffer, offset)[0]
 
 def asciz(s):
-    for length in range(len(s)):
-        if frombyte(s[length:length+1]) == 0:
-            break
-    return s[:length].decode("ASCII")
+    """Extract null-terminated string from bytes."""
+    if b'\0' in s:
+        s, _ = s.split(b'\0', 1)
+    return s.decode('ascii')
 
 
 def _read_fnt(fnt):
     """Create an internal font description from a .FNT-shaped string."""
     properties = {}
-    version = fromword(fnt[0:])
-    ftype = fromword(fnt[0x42:])
+    version = unpack(_WORD, fnt, 0)
+    ftype = unpack(_WORD, fnt, 0x42)
     if ftype & 1:
         raise ValueError('This font is a vector font')
-    off_facename = fromdword(fnt[0x69:])
+    off_facename = unpack(_LONG, fnt, 0x69)
     if off_facename < 0 or off_facename > len(fnt):
         raise ValueError('Face name not contained within font data')
     properties = {
         'source-format': 'WindowsFont [0x{:04x}]'.format(version),
         'name': asciz(fnt[off_facename:]),
         'copyright': asciz(fnt[6:66]),
-        'points': fromword(fnt[0x44:]),
-        'ascent': fromword(fnt[0x4A:]),
+        'points': unpack(_WORD, fnt, 0x44),
+        'ascent': unpack(_WORD, fnt, 0x4A),
     }
-    height = fromword(fnt[0x58:])
+    height = unpack(_WORD, fnt, 0x58)
     properties['size'] = height
-    properties['slant'] = 'italic' if frombyte(fnt[0x50:]) else 'roman'
+    properties['slant'] = 'italic' if unpack(_BYTE, fnt, 0x50) else 'roman'
     deco = []
-    if frombyte(fnt[0x51:]):
+    if unpack(_BYTE, fnt, 0x51):
         deco.append('underline')
-    if frombyte(fnt[0x52:]):
+    if unpack(_BYTE, fnt, 0x52):
         deco.append('strikethrough')
     if deco:
         properties['decoration'] = ' '.join(deco)
-    weight = fromword(fnt[0x53:])
+    weight = unpack(_WORD, fnt, 0x53)
     if weight:
         weight = max(100, min(900, weight))
         properties['weight'] = _WEIGHT_MAP[round(weight, -2)]
-    charset = frombyte(fnt[0x55:])
+    charset = unpack(_BYTE, fnt, 0x55)
     if charset in _CHARSET_MAP:
         properties['encoding'] = _CHARSET_MAP[charset]
     else:
@@ -182,24 +178,24 @@ def _read_fnt(fnt):
         ctsize = 6
     maxwidth = 0
     glyphs = {}
-    firstchar = frombyte(fnt[0x5F:])
-    lastchar = frombyte(fnt[0x60:])
+    firstchar = unpack(_BYTE, fnt, 0x5F)
+    lastchar = unpack(_BYTE, fnt, 0x60)
     for i in range(firstchar, lastchar+1):
         entry = ctstart + ctsize * (i-firstchar)
-        width = fromword(fnt[entry:])
+        width = unpack(_WORD, fnt, entry)
         if not width:
             continue
         if ctsize == 4:
-            off = fromword(fnt[entry+2:])
+            off = unpack(_WORD, fnt, entry+2)
         else:
-            off = fromdword(fnt[entry+2:])
+            off = unpack(_LONG, fnt, entry+2)
         rows = []
         bytewidth = ceildiv(width, 8)
         for j in range(height):
             rowbytes = []
             for k in range(bytewidth):
                 bytepos = off + k * height + j
-                rowbytes.append(frombyte(fnt[bytepos:]))
+                rowbytes.append(unpack(_BYTE, fnt, bytepos))
             rows.append(bytes_to_bits(rowbytes, width))
         if rows:
             glyphs[i] = Glyph(tuple(rows))
@@ -209,21 +205,20 @@ def _read_ne_fon(fon, neoff):
     """Finish splitting up a NE-format FON file."""
     ret = []
     # Find the resource table.
-    rtable = fromword(fon[neoff + 0x24:])
-    rtable = rtable + neoff
+    rtable = neoff + unpack(_WORD, fon, neoff + 0x24)
     # Read the shift count out of the resource table.
-    shift = fromword(fon[rtable:])
+    shift = unpack(_WORD, fon, rtable)
     # Now loop over the rest of the resource table.
-    p = rtable+2
-    while 1:
-        rtype = fromword(fon[p:])
+    p = rtable + 2
+    while True:
+        rtype = unpack(_WORD, fon, p)
         if rtype == 0:
             break  # end of resource table
-        count = fromword(fon[p+2:])
-        p = p + 8  # type, count, 4 bytes reserved
+        count = unpack(_WORD, fon, p+2)
+        p += 8  # type, count, 4 bytes reserved
         for i in range(count):
-            start = fromword(fon[p:]) << shift
-            size = fromword(fon[p+2:]) << shift
+            start = unpack(_WORD, fon, p) << shift
+            size = unpack(_WORD, fon, p+2) << shift
             if start < 0 or size < 0 or start+size > len(fon):
                 raise ValueError('Resource overruns file boundaries')
             if rtype == 0x8008: # this is an actual font
@@ -231,26 +226,28 @@ def _read_ne_fon(fon, neoff):
                     font = _read_fnt(fon[start:start+size])
                 except Exception as e:
                     raise ValueError('Failed to read font resource at {:x}: {}'.format(start, e))
-                ret = ret + [font]
-            p = p + 12 # start, size, flags, name/id, 4 bytes reserved
+                ret.append(font)
+            p += 12 # start, size, flags, name/id, 4 bytes reserved
     return ret
 
 def _read_pe_fon(fon, peoff):
     """Finish splitting up a PE-format FON file."""
-    dirtables=[]
-    dataentries=[]
-    def gotoffset(off,dirtables=dirtables,dataentries=dataentries):
+    dirtables = []
+    dataentries = []
+
+    def gotoffset(off, dirtables=dirtables, dataentries=dataentries):
         if off & 0x80000000:
-            off = off &~ 0x80000000
+            off &= ~0x80000000
             dirtables.append(off)
         else:
             dataentries.append(off)
+
     def dodirtable(rsrc, off, rtype, gotoffset=gotoffset):
-        number = fromword(rsrc[off+12:]) + fromword(rsrc[off+14:])
+        number = unpack(_WORD, rsrc, off+12) + unpack(_WORD, rsrc, off+14)
         for i in range(number):
             entry = off + 16 + 8*i
-            thetype = fromdword(rsrc[entry:])
-            theoff = fromdword(rsrc[entry+4:])
+            thetype = unpack(_LONG, rsrc, entry)
+            theoff = unpack(_LONG, rsrc, entry+4)
             if rtype == -1 or rtype == thetype:
                 gotoffset(theoff)
 
@@ -259,21 +256,20 @@ def _read_pe_fon(fon, peoff):
     # it's probably easiest just to go straight to the section table.
     # So let's find the size of the Optional Header, which we can
     # then skip over to find the section table.
-    secentries = fromword(fon[peoff+0x06:])
-    sectable = peoff + 0x18 + fromword(fon[peoff+0x14:])
+    secentries = unpack(_WORD, fon, peoff+0x06)
+    sectable = peoff + 0x18 + unpack(_WORD, fon, peoff+0x14)
     for i in range(secentries):
         secentry = sectable + i * 0x28
         secname = asciz(fon[secentry:secentry+8])
-        secrva = fromdword(fon[secentry+0x0C:])
-        secsize = fromdword(fon[secentry+0x10:])
-        secptr = fromdword(fon[secentry+0x14:])
-        if secname == ".rsrc":
+        secrva = unpack(_LONG, fon, secentry+0x0C)
+        secsize = unpack(_LONG, fon, secentry+0x10)
+        secptr = unpack(_LONG, fon, secentry+0x14)
+        if secname == '.rsrc':
             break
-    if secname != ".rsrc":
+    else:
         raise ValueError('Unable to locate resource section')
     # Now we've found the resource section, let's throw away the rest.
     rsrc = fon[secptr:secptr+secsize]
-
     # Now the fun begins. To start with, we must find the initial
     # Resource Directory Table and look up type 0x08 (font) in it.
     # If it yields another Resource Directory Table, we stick the
@@ -293,9 +289,9 @@ def _read_pe_fon(fon, peoff):
     # describes a font.
     ret = []
     for off in dataentries:
-        rva = fromdword(rsrc[off:])
+        rva = unpack(_LONG, rsrc, off)
+        size = unpack(_LONG, rsrc, off+4)
         start = rva - secrva
-        size = fromdword(rsrc[off+4:])
         try:
             font = _read_fnt(rsrc[start:start+size])
         except Exception as e:
@@ -305,11 +301,8 @@ def _read_pe_fon(fon, peoff):
 
 def _read_fon(fon):
     """Split a .FON up into .FNTs and pass each to _read_fnt."""
-    # Check the MZ header.
-    if fon[0:2] != b'MZ':
-        raise ValueError('MZ signature not found')
     # Find the NE header.
-    neoff = fromdword(fon[0x3C:])
+    neoff = unpack(_LONG, fon, 0x3C)
     if fon[neoff:neoff+2] == b'NE':
         return _read_ne_fon(fon, neoff)
     elif fon[neoff:neoff+4] == b'PE\0\0':
