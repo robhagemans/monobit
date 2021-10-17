a8b77243fafb1568e69547f194f066daa5e0b3ba
Rob Hagemans <rob.hagemans@hotmail.com>
2021-10-17 13:09:45 +0100

    add encoding, chart arguments to banner.py

--- banner.py
+++ banner.py
@@ -31,23 +31,40 @@ parser.add_argument('--fore', type=str, default='@')
 parser.add_argument('--back', type=str, default='-')
 parser.add_argument('--margin', type=pair, default=(0, 0))
 parser.add_argument('--scale', type=pair, default=(1, 1))
+parser.add_argument('--encoding', type=str, default='')
+parser.add_argument('--chart', action='store_true')
+
 args = parser.parse_args()
 
-
-# load, modify, save
 try:
-    if not args.text:
+    # codepage chart
+    if args.chart:
+        if args.text or args.encoding:
+            raise ValueError("Can't supply text or `--encoding` with `--chart`.")
+        args.text = b'\n'.join(bytes(range(_row, _row+16)) for _row in range(0, 256, 16))
+        args.encoding = '_'
+    # read text from stdin if not supplied
+    elif not args.text:
         args.text = sys.stdin.read()
     else:
-        # escape_decode is unofficial/unsupported
-        # https://stackoverflow.com/questions/4020539/process-escape-sequences-in-a-string-in-python
         args.text = unescape(args.text)
-        args.fore = unescape(args.fore)
-        args.back = unescape(args.back)
-    font = monobit.load(args.font)
+    # foreground and backgound characters
+    args.fore = unescape(args.fore)
+    args.back = unescape(args.back)
     # take first font from pack
-    if isinstance(font, monobit.Pack):
-        font, *_ = font
+    font, *_ = monobit.load(args.font)
+    # check if any characters are defined
+    # override encoding if requested
+    if not font.get_chars() and not args.encoding and not isinstance(args.text, bytes):
+        logging.warning(
+            'No character mappeing defined in font. Using `--encoding=raw` as fallback.'
+        )
+        args.encoding = 'raw'
+    if args.encoding == 'raw':
+        # use string as a representation of bytes
+        args.text = args.text.encode('latin-1', errors='ignore')
+    elif args.encoding:
+        font = font.set_properties(encoding=args.encoding)
     sys.stdout.write(to_text(font.render(
         args.text, args.fore, args.back, margin=args.margin, scale=args.scale, missing='default'
     )) + '\n')
