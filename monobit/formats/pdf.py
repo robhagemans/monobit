00e1919c20f59ee11aad1dedaa0eafb4d81a47dc
Rob Hagemans <rob.hagemans@hotmail.com>
2021-10-17 14:27:47 +0100

    improve presentation of pdf charts

--- monobit/formats/pdf.py
+++ monobit/formats/pdf.py
@@ -16,46 +16,61 @@ except ImportError:
 
 from ..formats import savers
 from ..streams import FileFormatError
+from ..label import Codepoint
+
 
 
 if reportlab:
+
     @savers.register('pdf')
     def save(
             fonts, outfile, where=None,
             format:str='png',
             columns:int=16,
             rows:int=16,
-            encoding:str=None,
         ):
         """Export font to pdf chart."""
         if len(fonts) > 1:
-            raise FileFormatError('Can only save one font to PDF file.')
+            raise FileFormatError('Can only export one font to PDF file.')
         font = fonts[0]
         canvas = Canvas(outfile)
         # assume A4
         page_x, page_y = 210*mm, 297*mm
         margin_x, margin_y = 28*mm, 30*mm
+        title_y = 5*mm
+        chart_width = page_x - 2*margin_x
+        chart_height = page_y - 2*margin_y - title_y
         # work with maximum raster size
         width = font.max_raster_size.x + 2
         height = font.max_raster_size.y + 4
-        xpix, ypix = (page_x-2*margin_x)/columns/width, (page_y-2*margin_y)/rows/height
+        xpix = chart_width / columns / width
+        ypix = chart_height /rows / height
         canvas.translate(margin_x, margin_y)
         canvas.setLineWidth(xpix/10)
         canvas.setStrokeColorRGB(0.5, 0.5, 0.5)
         canvas.setFillColorRGB(0, 0, 0)
+        canvas.setFont('Helvetica-Bold', title_y)
+        canvas.drawString(0, chart_height + title_y, font.name)
         canvas.setFont('Helvetica', ypix)
-        if encoding is not None:
-            #FIXME
-            pass
         for col in range(columns):
             for row in range(rows):
                 orig_x, orig_y = col*xpix*width, (rows-row-1)*ypix*height
                 n = columns * row + col
                 if n < len(font.glyphs):
-                    canvas.drawString(orig_x, orig_y+font.max_raster_size.y*ypix + 3, f'{n:02X}: {font.glyphs[n].char}')
-                    glyph = font.glyphs[n].as_matrix()
-                    for y in range(len(glyph)):
-                        for x in range(len(glyph[y])):
-                            canvas.rect(orig_x+x*xpix, orig_y+(len(glyph)-y-1)*ypix, xpix, ypix, fill=glyph[y][x])
+                    glyph = font.glyphs[n]
+                    label = (
+                        f'{Codepoint(glyph.codepoint)}: {glyph.char} '
+                        + ' '.join(
+                            f'[{_tag}]' for _tag in glyph.tags
+                        )
+                    )
+                    canvas.drawString(orig_x, orig_y+font.max_raster_size.y*ypix + 3, label)
+                    pixels = glyph.as_matrix()
+                    for y in range(len(pixels)):
+                        for x in range(len(pixels[y])):
+                            canvas.rect(
+                                orig_x+x*xpix, orig_y+(len(pixels)-y-1)*ypix, xpix, ypix,
+                                fill=pixels[y][x]
+                            )
         canvas.showPage()
         canvas.save()
