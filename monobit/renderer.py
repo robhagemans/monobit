6b3506711fa0afb392329bbb65f7ddf0c98ed545
Rob Hagemans <rob.hagemans@hotmail.com>
2021-10-17 19:44:37 +0100

    remove fore, back parameters from base rendering functions

--- monobit/renderer.py
+++ monobit/renderer.py
@@ -10,6 +10,15 @@ from .base.binary import ceildiv
 from .base.image import to_image
 
 
+# matrix colours
+# 0, 1 are background, foreground
+# this allows us to use max() to combine the three in blit_matrix
+_BORDER = -1
+
+
+###################################################################################################
+# text rendering
+
 def render_text(font, text, fore='@', back='-', *, margin=(0, 0), scale=(1, 1), missing='default'):
     """Render text string to text bitmap."""
     return to_text(
@@ -29,7 +38,7 @@ def render_image(
         fore=fore, back=back
     )
 
-def render(font, text, fore=1, back=0, *, margin=(0, 0), scale=(1, 1), missing='default'):
+def render(font, text, *, margin=(0, 0), scale=(1, 1), missing='default'):
     """Render text string to bitmap."""
     if isinstance(text, str):
         chars = [
@@ -81,7 +90,7 @@ def render(font, text, fore=1, back=0, *, margin=(0, 0), scale=(1, 1), missing='
     for row, kernrow in zip(glyphs, kernings):
         x, y = 0, 0
         for glyph, kerning in zip(row, kernrow):
-            matrix = glyph.as_matrix(1, 0)
+            matrix = glyph.as_matrix()
             # apply pre-offset so that x,y is logical coordinate of grid origin
             x, y = x + font.offset.x, y + font.offset.y
             # grid coordinates of grid origin
@@ -94,11 +103,11 @@ def render(font, text, fore=1, back=0, *, margin=(0, 0), scale=(1, 1), missing='
             x, y = x + font.tracking + kerning, y - font.offset.y
         grid_top += line_height
     scaled = _scale_matrix(canvas, *scale)
-    output = tuple(tuple(fore if _pix else back for _pix in _row) for _row in scaled)
-    return output
+    return scaled
 
 
 ###################################################################################################
+# matrix operations
 
 def _create_matrix(width, height, fill=0):
     """Create a matrix in list format."""
@@ -132,7 +141,7 @@ def _blit_matrix(matrix, canvas, grid_x, grid_y, operator=max):
 
 
 ###################################################################################################
-
+# glyph chart
 
 def chart_image(
         font,
@@ -156,7 +165,6 @@ def chart_text(
 def chart(
         font,
         columns=16, margin=(0, 0), padding=(0, 0), scale=(1, 1),
-        border=-1, back=0, fore=1,
     ):
     """Dump font to image."""
     scale_x, scale_y = scale
@@ -169,13 +177,13 @@ def chart(
     # determine image geometry
     width = columns * step_x + 2 * margin_x - padding_x
     height = rows * step_y + 2 * margin_y - padding_y
-    canvas = _create_matrix(width, height, border)
+    canvas = _create_matrix(width, height, _BORDER)
     # output glyphs
     for ordinal, glyph in enumerate(font.glyphs):
         if not glyph.width or not glyph.height:
             continue
         row, col = divmod(ordinal, columns)
-        matrix = glyph.as_matrix(fore, back)
+        matrix = glyph.as_matrix()
         matrix = _scale_matrix(matrix, scale_x, scale_y)
         left, bottom = margin_x + col*step_x, margin_y + (row+1)*step_y - padding_y
         _blit_matrix(matrix, canvas, left, bottom)
