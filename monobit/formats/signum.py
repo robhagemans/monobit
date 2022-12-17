82ab494bf5b1f7c80e742b0afc252c6d2d86b14f
Rob Hagemans <rob.hagemans@hotmail.com>
2022-12-17 10:36:13 +0000

    implement signum 9-pin and 24-pin printer formats

--- monobit/formats/signum.py
+++ monobit/formats/signum.py
@@ -21,23 +21,63 @@ from ..binary import ceildiv, bytes_to_bits
 _E24_MAGIC = b'eset0001'
 # printer fonts
 _L30_MAGIC = b'ls300001'
+_P24_MAGIC = b'ps240001'
+_P9_MAGIC = b'ps090001'
 
-# p24, p09, l30
+_SIGNUM_RANGE = range(127)
 
-_SIGNUM_RANGE = range(128)
+_parameters = {
+    'e24': dict(
+        fixed_byte_width=2,
+        # guess based on file contents
+        baseline=18,
+        line_height=24,
+        # just use standard here?
+        dpi=None,
+    ),
+    # below settings taken from
+    # https://github.com/Xiphoseer/sdo-tool/blob/main/crates/signum/src/chsets/printer.rs
+    'l30': dict(
+        fixed_byte_width=None,
+        baseline=48,
+        line_height=52,
+        dpi=(300, 300),
+    ),
+    'p24': dict(
+        fixed_byte_width=None,
+        baseline=58,
+        line_height=64,
+        dpi=(300, 300),
+    ),
+    'p9': dict(
+        fixed_byte_width=None,
+        baseline=36,
+        line_height=48,
+        dpi=(300, 300),
+    ),
+}
 
 
-@loaders.register('e24', name='signum', magic=(_E24_MAGIC,))
+
+@loaders.register('e24', name='signum-e24', magic=(_E24_MAGIC,))
 def load_signum(instream, where=None):
     """Load font from signum file."""
-    glyphs = _read_editor(instream)
-    return Font(glyphs)
+    return _read_signum(instream, **_parameters['e24'])
 
-@loaders.register('l30', name='signum-printer', magic=(_L30_MAGIC,))
+@loaders.register('l30', name='signum-l30', magic=(_L30_MAGIC,))
 def load_signum(instream, where=None):
     """Load font from signum file."""
-    glyphs = _read_printer(instream)
-    return Font(glyphs)
+    return _read_signum(instream, **_parameters['l30'])
+
+@loaders.register('p9', name='signum-p9', magic=(_P9_MAGIC,))
+def load_signum(instream, where=None):
+    """Load font from signum file."""
+    return _read_signum(instream, **_parameters['l30'])
+
+@loaders.register('p24', name='signum-p24', magic=(_P24_MAGIC,))
+def load_signum(instream, where=None):
+    """Load font from signum file."""
+    return _read_signum(instream, **_parameters['p24'])
 
 
 ################################################################################
@@ -46,7 +86,7 @@ def load_signum(instream, where=None):
 # file format reverse-engineered and documented by Xiphoseer
 # https://sdo.dseiler.eu/formats/eset
 
-_E24_HEADER = be.Struct(
+_HEADER = be.Struct(
     magic='8s',
     # font type, or number of chars? should be 128
     font_type='uint32',
@@ -68,21 +108,13 @@ _GLYPH_HEADER = be.Struct(
     reserved='uint8',
 )
 
-def _read_editor(instream):
-    """Read Signum! editor binary file and return glyphs."""
-    return _read_signum(instream, fixed_byte_width=2, top=24)
-
-def _read_printer(instream):
-    # top line relative to baseline
-    return _read_signum(instream, fixed_byte_width=None, top=48)
-
-def _read_signum(instream, fixed_byte_width=None, top=24):
+def _read_signum(instream, fixed_byte_width, baseline, line_height, dpi):
     """Read Signum! printer binary file and return glyphs."""
     data = instream.read()
-    header = _E24_HEADER.from_bytes(data)
+    header = _HEADER.from_bytes(data)
     ofs = header.size
     glyphs = []
-    for cp in range(127):
+    for cp in _SIGNUM_RANGE:
         glyph_header = _GLYPH_HEADER.from_bytes(data, ofs)
         ofs += glyph_header.size
         # printer fonts provide byte width in width field
@@ -106,22 +138,30 @@ def _read_signum(instream, fixed_byte_width=None, top=24):
             ofs += 1
             if pad:
                 notes += (' ' if notes else '') + f'padding=0x{pad:02x}'
-        glyph = Glyph.from_bytes(
-            glyph_bytes,
-            width=8*bytewidth,
-            codepoint=cp,
-            shift_up=top-glyph_header.top-glyph_header.height,
-            right_bearing=advance-8*bytewidth,
-            signum=notes if notes else None,
-        )
-        # as the width in printer fonts is given in bytes, not pixels,
-        # these seem to have no information about advance width.
-        # from actual files it looks reasonable to preserve only left bearing
-        # it is unclear how the advance width of whitespace is determined
-        # perhaps this is encoded in the unknown parts of the file header
-        glyph = glyph.crop(
-            right=glyph.padding.right,
-            adjust_metrics=bool(fixed_byte_width)
-        ).drop('shift-left')
+        if not glyph_bytes:
+            if not notes:
+                continue
+            glyph = Glyph(
+                codepoint=cp,
+                signum=notes if notes else None,
+            )
+        else:
+            glyph = Glyph.from_bytes(
+                glyph_bytes,
+                width=8*bytewidth,
+                codepoint=cp,
+                shift_up=baseline-glyph_header.top-glyph_header.height,
+                right_bearing=advance-8*bytewidth,
+                signum=notes if notes else None,
+            )
+            # as the width in printer fonts is given in bytes, not pixels,
+            # these seem to have no information about advance width.
+            # from actual files it looks reasonable to preserve only left bearing
+            # it is unclear how the advance width of whitespace is determined
+            # perhaps this is encoded in the unknown parts of the file header
+            glyph = glyph.crop(
+                right=glyph.padding.right,
+                adjust_metrics=bool(fixed_byte_width)
+            ).drop('shift-left')
         glyphs.append(glyph)
-    return glyphs
+    return Font(glyphs, line_height=line_height, dpi=dpi)
