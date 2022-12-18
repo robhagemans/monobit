3f44761a9070595e10120060eecd46467dda2692
Rob Hagemans <rob.hagemans@hotmail.com>
2022-12-18 21:27:55 +0000

    windows: move entry points to top of module

--- monobit/formats/windows.py
+++ monobit/formats/windows.py
@@ -41,6 +41,72 @@ from ..font import Font, Coord
 from ..glyph import Glyph
 
 
+@loaders.register(
+    #'fnt',
+    magic=(b'\0\x01', b'\0\x02', b'\0\x03'),
+    name='win-fnt',
+)
+def load_win_fnt(instream, where=None):
+    """Load font from a Windows .FNT resource."""
+    font = parse_fnt(instream.read())
+    return font
+
+@savers.register(linked=load_win_fnt)
+def save_win_fnt(fonts, outstream, where=None, version:int=2):
+    """
+    Save font to a Windows .FNT resource.
+
+    version: Windows font format version
+    """
+    if len(fonts) > 1:
+        raise FileFormatError('Can only save one font to Windows font resource.')
+    font = fonts[0]
+    outstream.write(create_fnt(font, version*0x100))
+    return font
+
+
+@loaders.register(
+    'fon',
+    magic=(b'MZ',),
+    name='win-fon',
+)
+def load_win_fon(instream, where=None):
+    """Load fonts from a Windows .FON container."""
+    data = instream.read()
+    mz_header = _MZ_HEADER.from_bytes(data)
+    if mz_header.magic not in (b'MZ', b'ZM'):
+        raise FileFormatError('MZ signature not found. Not a Windows .FON file')
+    ne_magic = data[mz_header.ne_offset:mz_header.ne_offset+2]
+    if ne_magic == b'NE':
+        fonts = _parse_ne(data, mz_header.ne_offset)
+    elif ne_magic == b'PE':
+        # PE magic should be padded by \0\0 but I'll believe it at this stage
+        fonts = _parse_pe(data, mz_header.ne_offset)
+    else:
+        raise FileFormatError(
+            'Executable signature is `{}`, not NE or PE. Not a Windows .FON file'.format(
+                ne_magic.decode('latin-1', 'replace')
+            )
+        )
+    fonts = [
+        font.modify(
+            source_format=font.source_format+' ({} FON container)'.format(ne_magic.decode('ascii'))
+        )
+        for font in fonts
+    ]
+    return fonts
+
+@savers.register(linked=load_win_fon)
+def save_win_fon(fonts, outstream, where=None, version:int=2):
+    """
+    Save fonts to a Windows .FON container.
+
+    version: Windows font format version
+    """
+    outstream.write(_create_fon(fonts, version*0x100))
+
+
+
 ##############################################################################
 # windows .FNT format definitions
 #
@@ -535,76 +601,6 @@ _IMAGE_RESOURCE_DATA_ENTRY = le.Struct(
     Reserved='dword',
 )
 
-
-##############################################################################
-# top level functions
-
-@loaders.register(
-    #'fnt',
-    magic=(b'\0\x01', b'\0\x02', b'\0\x03'),
-    name='win-fnt',
-)
-def load_win_fnt(instream, where=None):
-    """Load font from a Windows .FNT resource."""
-    font = parse_fnt(instream.read())
-    return font
-
-@savers.register(linked=load_win_fnt)
-def save_win_fnt(fonts, outstream, where=None, version:int=2):
-    """
-    Save font to a Windows .FNT resource.
-
-    version: Windows font format version
-    """
-    if len(fonts) > 1:
-        raise FileFormatError('Can only save one font to Windows font resource.')
-    font = fonts[0]
-    outstream.write(create_fnt(font, version*0x100))
-    return font
-
-
-@loaders.register(
-    'fon',
-    magic=(b'MZ',),
-    name='win-fon',
-)
-def load_win_fon(instream, where=None):
-    """Load fonts from a Windows .FON container."""
-    data = instream.read()
-    mz_header = _MZ_HEADER.from_bytes(data)
-    if mz_header.magic not in (b'MZ', b'ZM'):
-        raise FileFormatError('MZ signature not found. Not a Windows .FON file')
-    ne_magic = data[mz_header.ne_offset:mz_header.ne_offset+2]
-    if ne_magic == b'NE':
-        fonts = _parse_ne(data, mz_header.ne_offset)
-    elif ne_magic == b'PE':
-        # PE magic should be padded by \0\0 but I'll believe it at this stage
-        fonts = _parse_pe(data, mz_header.ne_offset)
-    else:
-        raise FileFormatError(
-            'Executable signature is `{}`, not NE or PE. Not a Windows .FON file'.format(
-                ne_magic.decode('latin-1', 'replace')
-            )
-        )
-    fonts = [
-        font.modify(
-            source_format=font.source_format+' ({} FON container)'.format(ne_magic.decode('ascii'))
-        )
-        for font in fonts
-    ]
-    return fonts
-
-@savers.register(linked=load_win_fon)
-def save_win_fon(fonts, outstream, where=None, version:int=2):
-    """
-    Save fonts to a Windows .FON container.
-
-    version: Windows font format version
-    """
-    outstream.write(_create_fon(fonts, version*0x100))
-
-
-##############################################################################
 
 ##############################################################################
 # windows .FNT reader
