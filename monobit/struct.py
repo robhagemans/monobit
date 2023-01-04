8ee7fcf73a0e0cb0bead56720e5f5bea259bb257
Rob Hagemans <rob.hagemans@hotmail.com>
2023-01-04 07:02:34 +0000

    create struct/ctype wrappers

--- monobit/struct.py
+++ monobit/struct.py
@@ -74,6 +74,8 @@ def _parse_type(atype):
     """Convert struct member type specification to ctypes base type or array."""
     if isinstance(atype, bitfield):
         return (*_parse_type(atype.type), atype.bits)
+    if isinstance(atype, NewType):
+        return _parse_type(atype._ctype)
     if isinstance(atype, type):
         return atype,
     try:
@@ -183,5 +185,143 @@ def _binary_types(parent):
         int32=_wrap_base_type(int32, parent),
     )
 
-big_endian = _binary_types(ctypes.BigEndianStructure)
-little_endian = _binary_types(ctypes.LittleEndianStructure)
+# big_endian = _binary_types(ctypes.BigEndianStructure)
+# little_endian = _binary_types(ctypes.LittleEndianStructure)
+
+
+
+class NewType:
+
+    def __mul__(self, count):
+        """Create an array."""
+        return self.array(count)
+
+    def from_bytes(self, *args):
+        return self._ctype.from_buffer_copy(*args)
+
+    def read_from(self, stream):
+        return self.from_bytes(stream.read(self.size))
+
+    def array(self, count):
+        return NewArray(self, count)
+
+    @property
+    def size(self):
+        return ctypes.sizeof(self._ctype)
+
+
+class NewSimple(NewType):
+
+    def __init__(self, endian, ctype):
+        if endian[:1].lower() in ('b', '>'):
+            self._ctype = ctype.__ctype_be__
+        elif endian[:1].lower() in ('l', '<'):
+            self._ctype = ctype.__ctype_le__
+        else:
+            raise ValueError(f"Endianness '{endian}' not recognised.")
+
+    def __call__(self, value):
+        """Instantiate a variable."""
+        # but bytes(...) woon't work correctly
+        return self._ctype(value).value
+
+    def from_bytes(self, *args):
+        return super().from_bytes(*args).value
+
+
+class NewStruct(NewType):
+
+    def __init__(self, endian, /, **description):
+        """Create a structured type."""
+        if endian[:1].lower() in ('b', '>'):
+            parent = ctypes.BigEndianStructure
+        elif endian[:1].lower() in ('l', '<'):
+            parent = ctypes.LittleEndianStructure
+        else:
+            raise ValueError(f"Endianness '{endian}' not recognised.")
+
+        fields = tuple(
+            (_field, *_parse_type(_type))
+            for _field, _type in description.items()
+        )
+
+        class Struct(parent):
+            """Struct with binary representation."""
+            _fields_ = fields
+            _pack_ = True
+
+            @property
+            def __dict__(self):
+                """Extract the fields dictionary."""
+                return dict(
+                    (field, getattr(self, field))
+                    for field, *_ in self._fields_
+                )
+
+            def __getattribute__(self, attr):
+                """Convert attributes where still necessary."""
+                value = super().__getattribute__(attr)
+                if isinstance(value, ctypes.Array):
+                    return tuple(value)
+                return value
+
+            # deprecate?
+            def __add__(self, other):
+                """Concatenate structs."""
+                addedstruct = NewStruct(endian, **dict(self._fields_), **dict(other._fields_))
+                return addedstruct(**vars(self), **vars(other))
+
+        self._ctype = Struct
+
+    def __call__(self, **kwargs):
+        """Instantiate a struct variable."""
+        return self._ctype(**kwargs)
+
+
+class NewArray(NewType):
+
+    def __init__(self, struct, count):
+        self._count = count
+        self._ctype = struct._ctype * count
+
+    def __call__(self, *args):
+        """Instantiate a struct variable."""
+        array = self._ctype
+        # deprecate?
+        print('here')
+        array.size = self.size
+        return array(*args)
+
+
+
+class NewStructBE(NewStruct):
+    def __init__(self, **description):
+        super().__init__('>', **description)
+
+class NewStructLE(NewStruct):
+    def __init__(self, **description):
+        super().__init__('<', **description)
+
+
+
+big_endian = SimpleNamespace(
+    Struct=NewStructBE,
+    char=NewSimple('>', char),
+    uint8=NewSimple('>', uint8),
+    int8=NewSimple('>', int8),
+    uint16=NewSimple('>', uint16),
+    int16=NewSimple('>', int16),
+    uint32=NewSimple('>', uint32),
+    int32=NewSimple('>', int32),
+)
+
+little_endian = SimpleNamespace(
+    Struct=NewStructLE,
+    char=NewSimple('<', char),
+    uint8=NewSimple('<', uint8),
+    int8=NewSimple('<', int8),
+    uint16=NewSimple('<', uint16),
+    int16=NewSimple('<', int16),
+    uint32=NewSimple('<', uint32),
+    int32=NewSimple('<', int32),
+)
