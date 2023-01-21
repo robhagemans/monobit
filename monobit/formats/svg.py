dd986203320844699239f956c2afd4492779a378
Rob Hagemans <rob.hagemans@hotmail.com>
2023-01-21 22:57:04 +0000

    implement svg vector ont reader

--- monobit/formats/svg.py
+++ monobit/formats/svg.py
@@ -7,13 +7,17 @@ licence: https://opensource.org/licenses/MIT
 
 import logging
 from math import ceil
+import xml.etree.ElementTree as etree
 
-from ..storage import savers
+from ..storage import loaders, savers
 from ..streams import FileFormatError
+from ..vector import StrokePath
+from ..font import Font
+
 
 _HEADER = """\
 <?xml version="1.0" standalone="no"?>
-<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd" >
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">
 <defs>
 """
@@ -23,10 +27,36 @@ _FOOTER = """\
 </svg>
 """
 
-@savers.register('svg', name='svg')
-def save_svg(
-        fonts, outfile, where=None,
-    ):
+
+@loaders.register('svg', name='svg')
+def load_svg(instream, where=None):
+    """Load vector font from Scalable Vector Graphics font."""
+    root = etree.parse(instream).getroot()
+    if not root.tag.endswith('svg'):
+        raise FileFormatError(f'Not an SVG file: root tag is {root.tag}')
+    root = root.find('{*}defs', namespaces='') or root
+    font = root.find('{*}font')
+    if not font:
+        raise FileFormatError('Not an SVG font file')
+    glyphdata = tuple(font.iterfind('{*}glyph'))
+    paths = tuple(
+        _g.attrib.get('d', '') or _g.find('{*}path').attrib.get('d', '')
+        for _g in glyphdata
+    )
+    chars = tuple(
+        _g.attrib.get('unicode', '')
+        for _g in glyphdata
+    )
+    glyphs = tuple(
+        # .shift(0, font.line_height-font.descent)
+        StrokePath.from_string(_path).flip().as_glyph(char=_char)
+        for _path, _char in zip(paths, chars)
+    )
+    return Font(glyphs)
+
+
+@savers.register(linked=load_svg)
+def save_svg(fonts, outfile, where=None):
     """Export vector font to Scalable Vector Graphics font."""
     if len(fonts) > 1:
         raise FileFormatError('Can only export one font to SVG file.')
@@ -43,7 +73,7 @@ def save_svg(
     outfile.write(f'<font-face font-family="{font.family}" units-per-em="{font.line_height}" />\n')
     for i, glyph in enumerate(font.glyphs):
         if glyph.path:
-            svgpath = glyph.path.flip().shift(0, font.line_height-font.descent).as_svg()
+            svgpath = StrokePath.from_string(glyph.path).flip().shift(0, font.line_height-font.descent).as_svg()
             d = f' d="{svgpath}"'
         else:
             d = ''
