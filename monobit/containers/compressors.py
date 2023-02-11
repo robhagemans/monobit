fa4db287c420565aa5300dad908761b9b10fca5e
Rob Hagemans <rob.hagemans@hotmail.com>
2023-02-11 21:13:06 +0000

    tidy up repetitive code in compressors module

--- monobit/containers/compressors.py
+++ monobit/containers/compressors.py
@@ -7,6 +7,7 @@ licence: https://opensource.org/licenses/MIT
 
 import logging
 from pathlib import Path
+from contextlib import contextmanager
 import gzip
 import lzma
 import bz2
@@ -63,57 +64,58 @@ class Compressor:
     must_have_magic = True
 
     @classmethod
-    def load(cls, instream, **kwargs):
-        """Load fonts from compressed stream."""
+    def _check_magic(cls, instream):
+        """Check if the magic signature is correct."""
         magic = instream.peek(len(cls.magic))
         if cls.must_have_magic and not magic.startswith(cls.magic):
             raise FileFormatError(
                 f'Not a {cls.name}-compressed file'
             )
-        name = Path(instream.name).stem
-        wrapped = Stream(
-            cls.compressor.open(instream, mode='rb'),
-            mode='r', name=name
-        )
-        try:
-            with wrapped:
-                return load_stream(wrapped, **kwargs)
-        except cls.error as e:
-            raise FileFormatError(e)
-
 
     @classmethod
-    def save(cls, fonts, outstream, **kwargs):
-        """Load fonts from compressed stream."""
-        name = Path(outstream.name).stem
-        wrapped = Stream(
-            cls.compressor.open(outstream, mode='wb'),
-            mode='w', name=name
-        )
-        try:
-            with wrapped:
-                return save_stream(fonts, wrapped, **kwargs)
-        except cls.error as e:
-            raise FileFormatError(e)
-
-    @classmethod
-    def open(cls, stream, mode, **kwargs):
-        """Open container on compressed stream."""
-        if mode == 'r':
-            magic = stream.peek(len(cls.magic))
-            if cls.must_have_magic and not magic.startswith(cls.magic):
-                raise FileFormatError(
-                    f'Not a {cls.name}-compressed file'
-                )
+    def _get_payload_stream(cls, stream, mode):
+        """Get the uncompressed stream."""
         name = Path(stream.name).stem
         wrapped = Stream(
             cls.compressor.open(stream, mode=mode + 'b'),
             mode=mode, name=name
         )
+        return wrapped
+
+    @classmethod
+    @contextmanager
+    def _translate_errors(cls):
+        """Context wrapper to convert library-specific errors to ours."""
         try:
-            container = open_container(wrapped, mode, **kwargs)
+            yield
         except cls.error as e:
             raise FileFormatError(e)
+
+    @classmethod
+    def load(cls, instream, **kwargs):
+        """Load fonts from compressed stream."""
+        cls._check_magic(instream)
+        wrapped = cls._get_payload_stream(instream, 'r')
+        with cls._translate_errors():
+            with wrapped:
+                return load_stream(wrapped, **kwargs)
+
+    @classmethod
+    def save(cls, fonts, outstream, **kwargs):
+        """Load fonts from compressed stream."""
+        wrapped = cls._get_payload_stream(outstream, 'w')
+        with cls._translate_errors():
+            with wrapped:
+                return save_stream(fonts, wrapped, **kwargs)
+
+    @classmethod
+    def open(cls, stream, mode, **kwargs):
+        """Open container on compressed stream."""
+        if mode == 'r':
+            cls._check_magic(stream)
+        wrapped = cls._get_payload_stream(stream, mode)
+        with cls._translate_errors():
+            container = open_container(wrapped, mode, **kwargs)
         return _WrappedContainer(container, stream)
 
     @classmethod
