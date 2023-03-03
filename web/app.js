41d98874aec334dbec9fe7b4315253e24474b731
Rob Hagemans <rob.hagemans@hotmail.com>
2023-03-03 21:32:05 +0000

    implement pyodide-based web app (remove old showfonts.html)

---
+++ web/app.js
@@ -0,0 +1,280 @@
+"use strict";
+
+// monobit web app - hoard of bitfonts
+// (c) 2023 Rob Hagemans
+// licence: https://opensource.org/licenses/MIT
+
+let pyodide = null;
+
+
+function setup() {
+    // reload code if listing changes
+    let listing = document.getElementById("listing0");
+    listing.onblur = showFont;
+    //setupHandlers();
+    setupFonts();
+    pyodide = setupPyodide();
+}
+
+
+///////////////////////////////////////////////////////////////////////////
+// font sample
+
+async function loadFont(element) {
+    let blob = await blobFromGithub(element);
+    let listing = document.getElementById("listing0");
+    listing.value = await blob.text();
+    document.getElementById("filename").value = element.path;
+    showFont();
+}
+
+
+function clearCanvas() {
+    let canvas = document.getElementById("sample");
+    let context = canvas.getContext("2d");
+    context.fillStyle = "black";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+async function showFont() {
+    clearCanvas();
+
+    let listing = document.getElementById("listing0");
+    let path = "/" + document.getElementById("filename").value.split("/").pop();
+    console.log(path);
+
+    let py = await pyodide;
+    py.FS.writeFile(path, listing.value);
+    py.globals.set("path", path);
+
+    let pycode = `if 1:
+    import js
+    import monobit
+
+    sample = b'\\n'.join(
+        bytes(range(_c, _c+32))
+        for _c in range(0, 256, 32)
+    )
+
+    sample = b'''...and the (quick & brown) fox jumps: over lazy dog #0123456789.
+THE QUICK, BROWN FOX JUMPS OVER A LAZY DOG!
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+culpa qui officia deserunt mollit anim id est laborum.'''
+
+    font, *_ = monobit.load(path)
+    #raster = monobit.render(font, sample, direction='ltr f')
+    raster = monobit.chart.chart(font, columns=32)
+
+    # scale for crisper result on JS canvas
+    raster = raster.stretch(2, 2)
+
+    # convert to RGBA vector
+    rgba = tuple(
+        (255, 255, 255, 255) if _e else (0, 0, 0, 255)
+        for _e in raster.as_vector()
+    )
+    rgba = bytes(_b for _e in rgba for _b in _e)
+    width = raster.width
+    height = raster.height
+    name = font.name
+    `
+    await py.runPython(pycode);
+
+    // show name as title
+    let title = document.getElementById("name");
+    title.innerHTML = py.globals.get("name");
+
+    // display on canvas
+    let array = new Uint8ClampedArray(
+        py.globals.get("rgba").toJs()
+    );
+    let imagedata = new ImageData(
+        array, py.globals.get("width"), py.globals.get("height")
+    );
+    let canvas = document.getElementById("sample");
+    let context = canvas.getContext("2d");
+
+    // resize canvas to fit width
+    canvas.width = imagedata.width;
+    canvas.height = Math.floor(imagedata.width * 3 / 4);
+    clearCanvas();
+
+    context.putImageData(imagedata, 0, 0);
+    canvas.focus();
+}
+
+
+///////////////////////////////////////////////////////////////////////////
+// event handlers
+//
+// function setupHandlers() {
+//     var app = apps[app_id];
+//
+//     // load files on drag & drop
+//     function nop(e) {
+//         e.stopPropagation();
+//         e.preventDefault();
+//     }
+//
+//     function drop(e) {
+//         e.stopPropagation();
+//         e.preventDefault();
+//         var files = e.dataTransfer.files;
+//         var reader = new FileReader();
+//         reader.onload = function() {
+//
+//             app.load(reader.result);
+//
+//         };
+//         reader.readAsText(files[0]);
+//     }
+//
+//     var element = app.canvas;
+//     element.addEventListener("dragenter", nop);
+//     element.addEventListener("dragover", nop);
+//     element.addEventListener("drop", drop);
+//
+//     var listing = document.getElementById("listing0");
+//     listing.addEventListener("dragenter", nop);
+//     listing.addEventListener("dragover", nop);
+//     listing.addEventListener("drop", drop);
+//
+//     var storage = document.getElementById("flop1");
+//     storage.addEventListener("dragenter", nop);
+//     storage.addEventListener("dragover", nop);
+//     storage.addEventListener("drop", nop);
+// }
+
+
+///////////////////////////////////////////////////////////////////////////////
+// font collection
+
+async function setupFonts() {
+    //
+    // retrieve font list from Github and show
+    //
+    let tree = null;
+    let refreshTime = JSON.parse(localStorage.getItem("refresh_time"));
+    // hit github no more than once per hour (rate limit)
+    if (refreshTime && Date.now() - refreshTime < 3.6e+6) {
+        tree = JSON.parse(localStorage.getItem("github_tree"));
+    }
+    if (!tree) {
+        console.log('refresh github tree');
+        let url = "https://api.github.com/repos/robhagemans/hoard-of-bitfonts/git/trees/master?recursive=1";
+        tree = await fetch(url)
+            .then((response) => response.json())
+            .then((result) => result.tree);
+        localStorage.setItem("github_tree", JSON.stringify(tree));
+        localStorage.setItem("refresh_time", JSON.stringify(Date.now()));
+    }
+    buildCollection(tree);
+}
+
+
+function buildCollection(collection) {
+    //
+    // show list of available fonts
+    //
+    let parent = document.getElementById("font-list");
+
+    let topli = document.createElement("li");
+    let list = document.createElement("ol");
+    parent.appendChild(document.createElement("ul")).appendChild(topli).appendChild(list);
+
+    for(let element of collection) {
+
+        if (element.type == "tree") {
+            topli = document.createElement("li");
+            topli.innerHTML = "&nbsp;&#x2605; " + element.path;
+
+            list = document.createElement("ol");
+            parent.appendChild(document.createElement("ul")).appendChild(topli).appendChild(list);
+        }
+        else {
+            // create download link to file
+            let a = document.createElement("a");
+            a.innerHTML = "&#9662;";
+            a.className = "hidden download";
+            a.onclick = () => { downloadFromGithub(element); return false; }
+
+            let play = document.createElement("a");
+            play.innerHTML = '<span class="hidden">&#9656;</span> ' + element.path;
+            play.className = "run";
+            play.onclick = () => { loadFont(element); return false; }
+
+            let li = list.appendChild(document.createElement("li"));
+            li.appendChild(a);
+            li.appendChild(play);
+        }
+    }
+}
+
+
+function downloadBytes(name, blob) {
+    //
+    // download binary content
+    //
+    //let blob = new Blob([bytes]);
+    // create another anchor and trigger download
+    let a = document.createElement("a");
+    a.className = "hidden download";
+    a.download = name;
+    a.href = window.URL.createObjectURL(blob);
+    // trigger download
+    a.click();
+    a.remove();
+}
+
+async function blobFromGithub(element) {
+    //
+    // get file from Github as blob
+    //
+    // use raw link instead of api link to avoid rate limit
+    let url = 'https://raw.githubusercontent.com/robhagemans/hoard-of-bitfonts/master/' + element.path;
+    let response = await fetch(url);
+    let blob = await response.blob();
+    return blob;
+}
+
+
+async function downloadFromGithub(element) {
+    //
+    // user download of file from Github
+    //
+    let blob = await blobFromGithub(element);
+    downloadBytes(element.path, blob);
+    // do not follow link
+    return false;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// pyodide
+
+async function setupPyodide() {
+    let pyodide = await loadPyodide();
+    await pyodide.loadPackage("micropip");
+    const micropip = pyodide.pyimport("micropip");
+    await Promise.all([
+        micropip.install("setuptools"),
+        micropip.install("lzma"),
+        micropip.install("monobit", /*keep_going*/ true, /*deps*/ false),
+    ]);
+    clearCanvas();
+    showFont();
+    return pyodide;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// kludge to use he #targets while keeping a top margin
+
+window.addEventListener("hashchange", function () {
+    window.scrollTo(0, 0);
+});
