c6a63c2eb5417ff4c4ded5fe528b03a95f6c0561
Rob Hagemans <rob.hagemans@hotmail.com>
2023-06-27 19:04:29 +0100

    add 2x3 blocks option

--- monobit/raster.py
+++ monobit/raster.py
@@ -8,7 +8,7 @@ licence: https://opensource.org/licenses/MIT
 import logging
 from itertools import zip_longest
 
-from .binary import ceildiv, reverse_by_group
+from .binary import ceildiv, reverse_by_group, bytes_to_bits
 from .basetypes import Bounds, Coord
 
 # sentinel object
@@ -51,6 +51,46 @@ class blockstr(str):
     """str that is shown as block text in interactive session."""
     def __repr__(self):
         return f'"""\\\n{self}"""'
+
+
+# block elements
+# this has nothing to do with `blockstr` above
+
+BLOCKS_2x2 = {
+    (0, 0, 0, 0): ' ',
+    (0, 0, 0, 1): '\u2597',
+    (0, 0, 1, 0): '\u2596',
+    (0, 0, 1, 1): '\u2584',
+    (0, 1, 0, 0): '\u259d',
+    (0, 1, 0, 1): '\u2590',
+    (0, 1, 1, 0): '\u259e',
+    (0, 1, 1, 1): '\u259f',
+    (1, 0, 0, 0): '\u2598',
+    (1, 0, 0, 1): '\u259a',
+    (1, 0, 1, 0): '\u258c',
+    (1, 0, 1, 1): '\u2599',
+    (1, 1, 0, 0): '\u2580',
+    (1, 1, 0, 1): '\u259c',
+    (1, 1, 1, 0): '\u259b',
+    (1, 1, 1, 1): '\u2588',
+}
+
+
+# sixel block elements in Unicode do not include full- and half-block elements defined elsewhere
+_SIXBITS = tuple(
+    tuple(1*_b for _b in bytes_to_bits((_code,))[2:])
+    for _code in range(1, 2**6-1)
+    if _code not in (0b010101, 0b101010)
+)
+BLOCKS_2x3 = {
+    (_0, _1, _2, _3, _4, _5): chr(0x1Fb00 + _i)
+    for _i, (_5, _4, _3, _2, _1, _0) in enumerate(_SIXBITS)
+} | {
+    (0, 0, 0, 0, 0, 0): ' ',
+    (0, 1, 0, 1, 0, 1): '\u2590',
+    (1, 0, 1, 0, 1, 0): '\u258c',
+    (1, 1, 1, 1, 1, 1): '\u2588',
+}
 
 
 # immutable bit matrix
@@ -203,6 +243,8 @@ class Raster:
         matrix = self.as_matrix()
         if resolution == (2, 2):
             return self._as_blocks_2x2(matrix)
+        if resolution == (2, 3):
+            return self._as_blocks_2x3(matrix)
         elif resolution == (1, 1):
             return self.as_text(ink='\u2588', paper=' ')
         raise ValueError(f'Unsupported block resolution {resolution}')
@@ -220,29 +262,37 @@ class Raster:
             )
             for _row, _next in zip_longest(matrix[::2], matrix[1::2], fillvalue=())
         )
-        blockdict = {
-            (0, 0, 0, 0): ' ',
-            (0, 0, 0, 1): '\u2597',
-            (0, 0, 1, 0): '\u2596',
-            (0, 0, 1, 1): '\u2584',
-            (0, 1, 0, 0): '\u259d',
-            (0, 1, 0, 1): '\u2590',
-            (0, 1, 1, 0): '\u259e',
-            (0, 1, 1, 1): '\u259f',
-            (1, 0, 0, 0): '\u2598',
-            (1, 0, 0, 1): '\u259a',
-            (1, 0, 1, 0): '\u258c',
-            (1, 0, 1, 1): '\u2599',
-            (1, 1, 0, 0): '\u2580',
-            (1, 1, 0, 1): '\u259c',
-            (1, 1, 1, 0): '\u259b',
-            (1, 1, 1, 1): '\u2588',
-        }
+        blockdict = BLOCKS_2x2
         quartets = '\n'.join(
             ''.join(blockdict[_quartet] for _quartet in _row)
             for _row in quartets
         )
         return blockstr(quartets + '\n')
+
+    @staticmethod
+    def _as_blocks_2x3(matrix):
+        """Convert glyph to a string of sixel block characters."""
+        sextets = tuple(
+            tuple(
+                _sextet
+                for _sextet in zip_longest(
+                    _row[::2], _row[1::2],
+                    _next[::2], _next[1::2],
+                    _three[::2], _three[1::2],
+                    fillvalue=0
+                )
+            )
+            for _row, _next, _three in zip_longest(
+                matrix[::3], matrix[1::3], matrix[2::3], fillvalue=()
+            )
+        )
+        blockdict = BLOCKS_2x3
+        blocks = '\n'.join(
+            ''.join(blockdict[_sextet] for _sextet in _row)
+            for _row in sextets
+        )
+        return blockstr(blocks + '\n')
+
 
 
     @classmethod
