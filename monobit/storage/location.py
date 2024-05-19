c45a6166c851af75987ec4ef926a0ead0d517ce9
Rob Hagemans <rob.hagemans@hotmail.com>
2024-05-19 09:54:45 +0100

    update open_location to accept open streams as input

--- monobit/storage/location.py
+++ monobit/storage/location.py
@@ -5,18 +5,41 @@ from monobit.storage.magic import FileFormatError, MagicRegistry
 from monobit.storage.containers.directory import Directory
 from monobit.base.struct import StructError
 
+from .streams import StreamBase, Stream, KeepOpen
 from .wrappers.compressors import WRAPPERS
 from .containers.container import CONTAINERS
 
 
-def open_location(location_spec, mode='r', format=''):
+def open_location(stream_or_location, mode='r', format=''):
     """
     Open stream on given location, resolving nested containers and wrappers.
     """
     if mode not in ('r', 'w'):
         raise ValueError(f"Unsupported mode '{mode}'.")
-    location = _ensure_location(location_spec)
-    return location.resolve(format)
+    if not stream_or_location:
+        raise ValueError(f'No location provided.')
+    if isinstance(stream_or_location, (str, Path, Location)):
+        location_spec = stream_or_location
+        if isinstance(location_spec, str):
+            location_spec = Path(location_spec)
+        if isinstance(location_spec, Path):
+            root = Path(location_spec.root)
+            subpath = location_spec.relative_to(root)
+            location = Location(Directory(root), subpath)
+        else:
+            location = location_spec
+        location = location.resolve(format)
+        return location
+    # stream_or_location is a file-like object
+    if isinstance(stream_or_location, StreamBase):
+        stream = stream_or_location
+    else:
+        # we didn't open the file, so we don't own it
+        # we neeed KeepOpen for when the yielded object goes out of scope in the caller
+        stream = Stream(KeepOpen(stream_or_location), mode=mode)
+    location = Location()
+    stream = location.unwrap_stream(stream, format)
+    return stream
 
 
 class Location:
@@ -51,26 +74,38 @@ class Location:
         # identify container/wrapper type on head
         stream = self.container.open(head, mode='r') #, mode, overwrite)
         self._open_streams.append(stream)
+        # FIXME may need multiple formats to unwrap
+        stream_or_container = self.unwrap_stream(stream, format=outer_format)
+        if isinstance(stream_or_container, StreamBase):
+            if not tail or str(tail) == '.':
+                return stream_or_container
+            # not a container but we still have a subpath - error
+            raise FileFormatError(
+                f"Cannot resolve subpath '{tail}' "
+                f"on non-container stream '{stream_or_container.name}'"
+            )
+        location = Location(stream_or_container, subpath=tail)
+        return location.resolve(format=new_format_spec)
+
+    def unwrap_stream(self, stream, format=''):
+        """
+        Open one or more wrappers until an unwrapped stream is found.
+        Returns Stream or Container object.
+        """
         while True:
             try:
-                unwrapped = _open_wrapper(stream, format=outer_format)
-                self._open_streams.append(unwrapped)
+                unwrapped = _open_wrapper(stream, format=format)
             except FileFormatError:
                 # not a wrapper
                 break
             else:
+                self._open_streams.append(unwrapped)
                 stream = unwrapped
         try:
-            container = _open_container(stream, format=outer_format)
+            return _open_container(stream, format=format)
         except FileFormatError:
-            # could not open as container
-            if str(tail) == '.':
-                # stream is a payload file
-                return stream
-            # not a container but we still have a subpath - error
-            raise
-        location = Location(container, subpath=tail)
-        return location.resolve(format=new_format_spec)
+            pass
+        return stream
 
 
 def _open_wrapper(instream, *, format='', **kwargs):
@@ -117,45 +152,6 @@ def _open_container(instream, *, format='', **kwargs):
     if format:
         message += f': format specifier `{format}` not recognised'
     raise FileFormatError(message)
-
-
-def _ensure_location(location_spec):
-    """Convert Path or str to Location object."""
-    if not location_spec:
-        raise ValueError(f'No location provided.')
-    if isinstance(location_spec, str):
-        location_spec = Path(location_spec)
-    if isinstance(location_spec, Path):
-        root = Path(location_spec.root)
-        subpath = location_spec.relative_to(root)
-        return Location(Directory(root), subpath)
-    elif isinstance(location_spec, Location):
-        return location_spec
-    raise TypeError(
-        f'location_spec must be Location, Path or str, not {type(location_spec)}'
-    )
-
-
-# @contextmanager
-# def open_location(location, mode):
-#     """Parse file specification, open stream."""
-#     if mode not in ('r', 'w'):
-#         raise ValueError(f"Unsupported mode '{mode}'.")
-#     if not location:
-#         raise ValueError(f'No location provided.')
-#     if isinstance(location, str):
-#         location = Path(location)
-#     if isinstance(location, Path):
-#         root = Path(location.root)
-#         subpath = location.relative_to(root)
-#         with DirectoryStream(root, mode) as stream:
-#             yield stream, subpath
-#     elif isinstance(location, StreamBase):
-#         yield location, ''
-#     else:
-#         # we didn't open the file, so we don't own it
-#         # we neeed KeepOpen for when the yielded object goes out of scope in the caller
-#         yield Stream(KeepOpen(location), mode=mode), ''
 
 
 
