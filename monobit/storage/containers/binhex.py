59e9c30385590c74596ce86227c5393537c8a2e7
Rob Hagemans <rob.hagemans@hotmail.com>
2024-06-13 22:44:19 +0100

    implement binscii reader

--- monobit/storage/containers/binhex.py
+++ monobit/storage/containers/binhex.py
@@ -8,8 +8,9 @@ licence: https://opensource.org/licenses/MIT
 import logging
 from binascii import crc_hqx
 from itertools import zip_longest
+from functools import partial
 
-from monobit.base.struct import big_endian as be
+from monobit.base.struct import big_endian as be, little_endian as le
 from ..streams import Stream
 from ..magic import FileFormatError, Magic
 from ..base import containers
@@ -36,6 +37,26 @@ _CRC = be.Struct(
     crc='uint16',
 )
 
+def sixbitdecode(encoded, codedict, byteswap=False):
+    """Deocde six-bit ascii representation into bytes."""
+    # decode into 6-bit ints
+    try:
+        data = (codedict[_c] for _c in encoded)
+    except KeyError as e:
+        raise FileFormatError(f"Unexpected character '{e}' in encoding.") from e
+    if byteswap:
+        # reverse in groups of 4 code bytes, for binscii
+        args = [iter(data)] * 4
+        data = b''.join(bytearray(reversed(_chunk)) for _chunk in zip(*args))
+    # convert to bit sequence
+    bits = ''.join(bin(_d)[2:].zfill(6) for _d in data)
+    # group into chunks of 8
+    args = [iter(bits)] * 8
+    octets = (''.join(_t) for _t in zip_longest(*args, fillvalue='0'))
+    # convert to bytes
+    bytestr = bytes(int(_s, 2) for _s in octets)
+    return bytestr
+
 
 @containers.register(
     name='binhex',
@@ -57,15 +78,7 @@ class BinHex(FlatFilterContainer):
         if back:
             logging.warning('Additional data found after BinHex section: %r', back)
         binhex = ''.join(binhex.split('\n'))
-        # decode into 6-bit ints
-        data = (_BINHEX_CODEDICT[_c] for _c in binhex)
-        # convert to bit sequence
-        bits = ''.join(bin(_d)[2:].zfill(6) for _d in data)
-        # group into chunks of 8
-        args = [iter(bits)] * 8
-        octets = (''.join(_t) for _t in zip_longest(*args, fillvalue='0'))
-        # convert to bytes
-        bytestr = bytes(int(_s, 2) for _s in octets)
+        bytestr = sixbitdecode(binhex, _BINHEX_CODEDICT)
         # find run-length encoding marker
         chunks = bytestr.split(b'\x90')
         out = bytearray(chunks[0])
@@ -117,3 +130,77 @@ class BinHex(FlatFilterContainer):
                 f'data': data,
                 f'rsrc': rsrc,
             }
+
+
+###############################################################################
+
+_BINSCII_HEADER = le.Struct(
+    filesize='3s',
+    segstart='3s',
+    acmode='byte',
+    filetype='byte',
+    auxtype='word',
+    storetype='byte',
+    blksize='word',
+    credata='word',
+    cretime='word',
+    moddate='word',
+    modtime='word',
+    seglen='3s',
+    crc='word',
+    filler='byte',
+)
+
+@containers.register(
+    name='binscii',
+    magic=(
+        # this can occur on any line, probably not the first...
+        b'FiLeStArTfIlEsTaRt',
+    ),
+    patterns=('*.bsc', '*.bsq', '*.bns'),
+)
+class BinSCII(FlatFilterContainer):
+    """BinSCII loader."""
+
+    # based on https://mirrors.apple2.org.za/ground.icaen.uiowa.edu/Mirrors/uni-kl/doc/binscii.format
+
+    # TODO multi-chunk files
+
+    def decode_all(self, instream):
+        """Parse a BinSCII file."""
+        instream = instream.text
+        # locate binscii section sentinel
+        for line in instream:
+            if line.strip() == 'FiLeStArTfIlEsTaRt':
+                break
+        else:
+            raise FileFormatError(
+                f"No BinSCII section found in file '{instream.name}'."
+            )
+        encoding = instream.readline().strip()
+        codedict = {_c: _i for _i, _c in enumerate(encoding)}
+        metadata = instream.readline().strip()
+        name_len = encoding.index(metadata[0]) + 1
+        name = metadata[1:16]
+        name = name[:name_len]
+        binsciidecode = partial(sixbitdecode, codedict=codedict, byteswap=True)
+        headerbytes = binsciidecode(metadata[16:16+36])
+        header = _BINSCII_HEADER.from_bytes(headerbytes)
+        print(header)
+        header_crc = crc_hqx(headerbytes[:-3], 0)
+        if header_crc != header.crc:
+            logging.warning(f"CRC failure while decoding '{name}'")
+        decoded = []
+        crcline = ''
+        for dataline in instream:
+            dataline = dataline.strip()
+            if len(dataline) != 64:
+                crcline = dataline
+                break
+            decoded.append(binsciidecode(dataline))
+        data = b''.join(decoded)
+        crc_bytes = binsciidecode(crcline)
+        crc_target = int.from_bytes(crc_bytes[:2], byteorder='little')
+        if crc_hqx(data, 0) != crc_target:
+            logging.warning(f"CRC failure while decoding '{name}'")
+        return {name: data}
