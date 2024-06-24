b21226b302888669360501c8b761a49764135264
Rob Hagemans <rob.hagemans@hotmail.com>
2024-06-24 19:48:05 +0100

    implement plus3dos writer

--- monobit/storage/formats/raw/nearlyraw.py
+++ monobit/storage/formats/raw/nearlyraw.py
@@ -8,7 +8,7 @@ licence: https://opensource.org/licenses/MIT
 import logging
 
 from monobit.storage import loaders, savers, Magic, FileFormatError
-from monobit.core import Glyph, Font
+from monobit.core import Glyph, Font, Char
 from monobit.base.struct import little_endian as le, bitfield
 
 from .raw import load_bitmap, save_bitmap
@@ -539,6 +539,8 @@ _PLUS3DOS_HEADER = le.Struct(
     reserved=le.uint8 * 104,
     checksum='byte',
 )
+
+
 @loaders.register(
     name='plus3dos',
     magic=(_PLUS3DOS_MAGIC,),
@@ -564,3 +566,36 @@ def load_plus3dos(instream):
     font = font.modify(source_format='+3DOS')
     font = font.label(char_from='ascii')
     return font
+
+
+@savers.register(linked=load_plus3dos)
+def save_plus3dos(fonts, outstream):
+    """Save a 768-byte raw font with +3DOS header."""
+    font, *extra = fonts
+    if extra:
+        raise ValueError('Can only save a single font to a +3DOS file')
+    if font.spacing != 'character-cell' or font.cell_size != (8, 8):
+        raise ValueError(
+            'This format can only store 8x8 character-cell fonts'
+        )
+    header = _PLUS3DOS_HEADER(
+        signature=_PLUS3DOS_MAGIC,
+        issue=1,
+        version=0,
+        file_size=768 + _PLUS3DOS_HEADER.size,
+        file_type=3,
+        data_length=768,
+        # following psftools in the choice for these:
+        # load address
+        param_1=0x3d00,
+        unused=ord('f'),
+    )
+    header.checksum = sum(bytes(header)) & 0xff
+    font = font.resample(
+        # need explicit Char as to_label does surprising (in this context) things
+        # with numeric strings like '1'
+        chars=(Char(chr(_c)) for _c in range(32, 128)),
+        missing=font.get_glyph(' '),
+    )
+    outstream.write(bytes(header))
+    save_bitmap(outstream, font)
