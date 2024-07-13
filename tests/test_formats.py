10f23a849b9c1bce2ee9a3e1450ce0a8a0e2fefd
Rob Hagemans <rob.hagemans@hotmail.com>
2024-07-13 08:23:26 +0100

    add test for wsfont writer

--- tests/test_formats.py
+++ tests/test_formats.py
@@ -1022,6 +1022,16 @@ class TestFormats(BaseTester):
         self.assertEqual(len(font.glyphs), 255)
         self.assertEqual(font.get_glyph(b'A').reduce().as_text(), self.fixed4x6_A)
 
+    # wsfont
+
+    def test_export_wsfont(self):
+        """Test exporting wsfont files."""
+        fnt_file = self.temp_path / '4x6.wsf'
+        monobit.save(self.fixed4x6, fnt_file)
+        font, *_ = monobit.load(fnt_file)
+        self.assertEqual(len(font.glyphs), 256)
+        self.assertEqual(font.get_glyph('A').reduce().as_text(), self.fixed4x6_A)
+
     # COM loaders
 
     def test_import_frapt(self):
