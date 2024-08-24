53d1e31dd6682a9a96ec0b94422ff8ae69f532c4
Rob Hagemans <rob.hagemans@hotmail.com>
2024-08-24 11:35:22 +0100

    add test exercising text and block chart functions

--- tests/test_chart.py
+++ tests/test_chart.py
@@ -12,13 +12,23 @@ from .base import BaseTester, assert_text_eq
 
 class TestChart(BaseTester):
 
-    # PDF chart
-
     def test_export_pdf(self):
         """Test exporting pdf files."""
         pdf_file = self.temp_path / '4x6.pdf'
         monobit.save(self.fixed4x6, pdf_file)
         self.assertTrue(os.path.getsize(pdf_file) > 0)
+
+    def test_export_chart(self):
+        """Test exporting text chart."""
+        txt_file = self.temp_path / '4x6.txt'
+        monobit.save(self.fixed4x6, txt_file, format='chart')
+        self.assertTrue(os.path.getsize(txt_file) > 0)
+
+    def test_export_blocks(self):
+        """Test exporting blocks chart."""
+        txt_file = self.temp_path / '4x6.txt'
+        monobit.save(self.fixed4x6, txt_file, format='blocks')
+        self.assertTrue(os.path.getsize(txt_file) > 0)
 
 
 if __name__ == '__main__':
