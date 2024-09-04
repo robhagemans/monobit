7806c8d5993d4c34292f812bb6944736a0343053
Rob Hagemans <rob.hagemans@hotmail.com>
2024-09-04 19:46:22 +0100

    fix chunk pointers in zapredraw writer

--- monobit/storage/fontformats/zapredraw.py
+++ monobit/storage/fontformats/zapredraw.py
@@ -72,9 +72,8 @@ def load_zapredraw(instream):
     logging.debug(chunk_dir)
     glyphs = []
     if header.null_offset:
-        # we are already at the point indicated by null_offset - no need to seek
-        logging.debug(instream.tell())
-        logging.debug(header.null_offset)
+        # we should already be at the point indicated by null_offset
+        instream.seek(header.null_offset)
         part_font = load_bitmap(
             instream, width=header.width, height=header.height,
             count=1,
@@ -92,11 +91,11 @@ def load_zapredraw(instream):
         # note that "character offset" here means the character's ordinal in the list
         # so if the first defined character has codepoint 3, the next one codepoint 5,
         # the translation table starts \0\0\0\1\0\2 ...
-        logging.debug(instream.tell())
-        logging.debug(entry.offset)
+        #
+        # we should already be at the point indicated by offset
+        instream.seek(entry.offset)
         if entry.count < 256:
             translation_table = instream.read(256)
-            logging.debug(translation_table)
             codepoints = sorted(
                 _cp
                 for _cp, _offs in enumerate(translation_table)
@@ -105,10 +104,9 @@ def load_zapredraw(instream):
         else:
             codepoints = range(256)
         part_font = load_bitmap(
-            instream, width=header.width, height=header.height,
-            count=entry.count,
-            msb='right', align='right',
-            byte_swap=ceildiv(header.width, 8),
+            instream,
+            width=header.width, height=header.height, count=entry.count,
+            msb='right', align='right', byte_swap=ceildiv(header.width, 8),
         )
         glyphs.extend(
             _g.modify(codepoint=entry.chunk_number+_cp)
@@ -131,7 +129,6 @@ def save_zapredraw(fonts, outstream):
     """Save a !ZapRedraw UCS font."""
     font = ensure_single(fonts)
     font = ensure_charcell(font)
-    font = font.label()
     # determine number of chunks:
     # first bytes of 16-bit unicode codepoints
     chunked_codepoints = tuple(sorted(
@@ -168,7 +165,7 @@ def save_zapredraw(fonts, outstream):
     )]
     offset = header.null_offset + glyph_size + 256 * glyph_size
     # other chunks
-    for chunk, codepoints, _ in chunk_map[1:]:
+    for chunk, codepoints, translation_table in chunk_map[1:]:
         chunk_dir.append(
             _CHUNK_ENTRY(
                 chunk_number=chunk*0x100,
@@ -176,7 +173,7 @@ def save_zapredraw(fonts, outstream):
                 offset=offset,
             )
         )
-        offset += len(codepoints) * glyph_size
+        offset += len(translation_table) + len(codepoints) * glyph_size
     chunk_dir = (_CHUNK_ENTRY * n_chunks)(*chunk_dir)
     # write out
     outstream.write(bytes(header))
