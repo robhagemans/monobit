599f784ebf077c176dce3b1ff82cea56bd3a9488
Rob Hagemans <rob.hagemans@hotmail.com>
2026-06-11 18:44:55 +0100

    lazy-load encoding registry

--- monobit/encoding/definitions.py
+++ monobit/encoding/definitions.py
@@ -40,41 +40,58 @@ def register_charmaps(charmaps):
         charmaps[aliases] = charmap
 
 
-encodings = EncodingRegistry()
+class LazyEncodingRegistry():
 
-# unicode aliases
-encodings['unicode', 'ucs', 'iso10646', 'iso10646-1'] = Unicode()
+    _registry = None
 
-register_charmaps(encodings)
+    for _dunder in ('__getitem__', '__setitem__', '__iter__', '__getattr__'):
 
+        def _delegated(self, *args, **kwargs):
+            if self._registry is None:
+                self._build()
+            fn = getattr(self._registry, _dunder)
+            return fn(*args, **kwargs)
+
+
+    def _build(self):
+        # unicode aliases
+        encodings = EncodingRegistry()
+
+        encodings['unicode', 'ucs', 'iso10646', 'iso10646-1'] = Unicode()
+
+        register_charmaps(encodings)
+
+        for tagmap in (
+                CharTagger(),
+                CodepointTagger(),
+                UnicodeNameTagger(),
+                DescriptionTagger(),
+            ):
+            encodings[tagmap.name] = tagmap
+
+
+        encodings['adobe'] = EncoderLoader(
+            'agl/aglfn.txt', name='adobe', format='tagmap',
+            separator=';', unicode_column=0, tag_column=1,
+            fallback=AdobeFallbackTagger()
+        )
+
+        encodings['sgml'] = EncoderLoader(
+            'misc/SGML.TXT', name='sgml', format='tagmap',
+            separator='\t', unicode_column=2,
+            fallback=SGMLFallbackTagger()
+        )
+
+        # truetype mapping is adobe mapping *but* with .null for NUL
+        # https://developer.apple.com/fonts/TrueType-Reference-Manual/RM06/Chap6post.html
+        encodings['truetype'] = EncoderLoader(
+            'agl/aglfn.txt', name='truetype', format='tagmap',
+            separator=';', unicode_column=0, tag_column=1,
+            fallback=AdobeFallbackTagger()
+        ) | Tagmap({'\0': '.null'})
+
+        self._registry = encodings
+
+encodings = LazyEncodingRegistry()
 
 ###############################################################################
-
-for tagmap in (
-        CharTagger(),
-        CodepointTagger(),
-        UnicodeNameTagger(),
-        DescriptionTagger(),
-    ):
-    encodings[tagmap.name] = tagmap
-
-
-encodings['adobe'] = EncoderLoader(
-    'agl/aglfn.txt', name='adobe', format='tagmap',
-    separator=';', unicode_column=0, tag_column=1,
-    fallback=AdobeFallbackTagger()
-)
-
-encodings['sgml'] = EncoderLoader(
-    'misc/SGML.TXT', name='sgml', format='tagmap',
-    separator='\t', unicode_column=2,
-    fallback=SGMLFallbackTagger()
-)
-
-# truetype mapping is adobe mapping *but* with .null for NUL
-# https://developer.apple.com/fonts/TrueType-Reference-Manual/RM06/Chap6post.html
-encodings['truetype'] = EncoderLoader(
-    'agl/aglfn.txt', name='truetype', format='tagmap',
-    separator=';', unicode_column=0, tag_column=1,
-    fallback=AdobeFallbackTagger()
-) | Tagmap({'\0': '.null'})
