38b548fc09fd2607d2612d523a6513eac4ba2717
Rob Hagemans <rob.hagemans@hotmail.com>
2026-08-16 21:03:16 +0100

    fix date notice on printshop

--- monobit/storage/fontformats/printshop.py
+++ monobit/storage/fontformats/printshop.py
@@ -1,7 +1,7 @@
 """
 monobit.storage.fontformats.printshop - Broderbund's The Print Shop font files
 
-(c) 2023 Rob Hagemans
+(c) 2023--2026 Rob Hagemans
 licence: https://opensource.org/licenses/MIT
 """
 
