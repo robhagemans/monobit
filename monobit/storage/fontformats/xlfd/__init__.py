a19f25892405d574ef5b833eb4c1f67c3b57d723
Rob Hagemans <rob.hagemans@hotmail.com>
2026-08-23 21:11:59 +0100

    move xlfd/* -> unix/

--- monobit/storage/fontformats/xlfd/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-monobit.storage.fontformats.xlfd - formats based on X Logical Font Description
-
-(c) 2019--2026 Rob Hagemans
-licence: https://opensource.org/licenses/MIT
-"""
-
-from monobit.base import import_all
-import_all(__name__)
